ad83803bb3bc4c2c804287f18292c93b8c72120f
nikita-grabek <nikita.grabek@magenta.de>
2023-05-27 15:46:47 +0200

    added capacitor

---
+++ phantasiename/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.phatasiename.app',
+  appName: 'phantasiename',
+  webDir: 'dist/phantasiename',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
